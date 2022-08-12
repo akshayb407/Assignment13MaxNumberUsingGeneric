323158667f6af259745d0108918c7fc598ee40e8
akshayb407 <akshayb407@gmail.com>
2022-08-12 15:38:19 +0530

    .gitignore

---
+++ MaxNumberUsingGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaxNumberUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Max Number Program!");
+        }
+    }
+}
