88ca304f486e431fde329f5745a826f5fae47c16
akshayb407 <akshayb407@gmail.com>
2022-08-12 16:21:13 +0530

    TC1.1

---
+++ MaxNumberUsingGenerics/MaximumNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumberUsingGenerics
+{
+    internal class MaximumNumber
+    {
+        public int ReturnMaxNumber(int num1, int num2, int num3)
+        {
+            int temp;
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+
+            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                temp = num1;
+                num1 = num2;
+                num2 = temp;
+
+                return num1;
+            }
+
+            else
+            {
+                temp = num1;
+                num1 = num3;
+                num3 = temp;
+
+                return num1;
+            }
+        }
+    }
+}
--- MaxNumberUsingGenerics/Program.cs
+++ MaxNumberUsingGenerics/Program.cs
@@ -8,12 +8,13 @@ namespace MaxNumberUsingGenerics
         {
             Console.WriteLine("Welcome to Max Number Program!");
             Console.WriteLine("Find Maximum Number");
-            int num1 = 20;
-            int num2 = 25;
+
+            int num1 = 10;
+            int num2 = 20;
             int num3 = 30;
-            GenericMaximum<int> maximum = new GenericMaximum<int>(num1, num2, num3);
-            int max = maximum.MaxMethod();
-            Console.WriteLine(max);
+            MaximumNumber max = new MaximumNumber();
+            int maxNumber = max.ReturnMaxNumber(num1, num2, num3);
+            Console.WriteLine("max number is:" + maxNumber);
 
         }
     }
