4dad2bbd464fde090e7b4aa6dc7cf7437d4bc12a
akshayb407 <akshayb407@gmail.com>
2022-08-12 16:13:20 +0530

    UC1

---
+++ MaxNumberUsingGenerics/GenericMaximum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MaxNumberUsingGenerics
+{
+    internal class GenericMaximum<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+
+        public GenericMaximum(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+
+        public static T MaxValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else
+            {
+                return thirdValue;
+            }
+        }
+
+        public T MaxMethod()
+        {
+            T max = GenericMaximum<T>.MaxValue(this.firstValue, this.secondValue, this.thirdValue);
+            return max;
+        }
+
+    }
+}
--- MaxNumberUsingGenerics/Program.cs
+++ MaxNumberUsingGenerics/Program.cs
@@ -7,6 +7,14 @@ namespace MaxNumberUsingGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Max Number Program!");
+            Console.WriteLine("Find Maximum Number");
+            int num1 = 20;
+            int num2 = 25;
+            int num3 = 30;
+            GenericMaximum<int> maximum = new GenericMaximum<int>(num1, num2, num3);
+            int max = maximum.MaxMethod();
+            Console.WriteLine(max);
+
         }
     }
 }
